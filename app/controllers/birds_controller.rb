ed95afa2a9034eefa8413a7c2bd600ba4ff09b2e
MurituMaina <vincent.maina@student.moringaschool.com>
2022-11-25 09:22:40 +0300

    commit tree 57a18c57a16766909cd0d100e36c5a6e1c9781d8

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -1,6 +1,7 @@
 class BirdsController < ApplicationController
   rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
-
+  # add rescue_from
+  rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
   # GET /birds
   def index
     birds = Bird.all
@@ -9,8 +10,15 @@ class BirdsController < ApplicationController
 
   # POST /birds
   def create
-    bird = Bird.create(bird_params)
+    bird = Bird.create!(bird_params)
     render json: bird, status: :created
+  # rescue ActiveRecord::RecordInvalid => invalid
+  #   render json: {errors: invalid.record.errors}, status: :unprocessable_entity
+    # if bird.valid?
+    # render json: bird, status: :created
+    # else
+    # render json: {errors: bird.errors}, status: :unprocessable_entity
+    # end
   end
 
   # GET /birds/:id
@@ -22,8 +30,10 @@ class BirdsController < ApplicationController
   # PATCH /birds/:id
   def update
     bird = find_bird
-    bird.update(bird_params)
+    bird.update!(bird_params)
     render json: bird
+  # rescue ActiveRecord::RecordInvalid => invalid
+  #   render json: {errors: invalid.record.errors}, status: :unprocessable_entity
   end
 
   # DELETE /birds/:id
@@ -35,6 +45,10 @@ class BirdsController < ApplicationController
 
   private
 
+  def render_unprocessable_entity_response(invalid)
+  # render json: {errors: invalid.record.errors}, status: :unprocessable_entity
+  render json: { errors: invalid.record.errors.full_messages }, status: :unprocessable_entity
+  end
   def find_bird
     Bird.find(params[:id])
   end
